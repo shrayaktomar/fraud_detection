c4fc4675eb3423425b3ee4684a91ce06c51d4bcd
shrayaktomar <shrayaktomar@gmail.com>
2021-12-24 17:34:59 +0530

    This is the 4th commit

--- data_processing.py
+++ data_processing.py
@@ -2,3 +2,10 @@ print("this is a processing database")
 def sum(a,b):
     return a+b
 print("the sum of 5 and 6 ",sum(5,6))
+def sub(a,b):
+    return a-b
+print("the sum of 5 and 6",sub(6,5))
+
+def mul(a,b):
+    return b
+print("the multiplication of 5 and 6 is ",mul(5,6))
