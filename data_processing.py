122db0d1d77c2181631959018a6f08f648be7776
shrayaktomar <shrayaktomar@gmail.com>
2021-12-24 17:55:27 +0530

    this is the 6th commit and thank god for it basically basic maths of 5 and 6

--- data_processing.py
+++ data_processing.py
@@ -4,3 +4,9 @@ def sum(a,b):
 print("the sum of 5 and 6 ",sum(5,6))
 def sub(a,b):
     print("the diiference between 5 and 6 is ",sub(6,5))
+def mul(a,b):
+    return a*b
+print("the multiplication of the number 5 and 6  is ",mul(5,6))
+def div(a,b):
+    return b/a
+print("the divison of the value 5 and 6 is "div(5,6))
